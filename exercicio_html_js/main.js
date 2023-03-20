78a6aa2a47bc9ea829dd48efa91e74b63d65b01b
Edmilton Santos <edmiltonsnts@gmail.com>
2023-03-20 13:40:59 -0300

    new files

---
+++ exercicio_html_js/main.js
@@ -0,0 +1,22 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+    
+    var valor1 = document.getElementById('valor1').value;
+    var valor2 = document.getElementById('valor2').value;
+    
+    
+    if (valor1 > valor2) {
+      resultado = "Formulário inválido o valor 1 é maior que o valor 2.";
+    } else if (valor2 > valor1) {
+      resultado = "O formulário e válido pois o valor 2 é maior que o valor 1.";
+    } else {
+      resultado = "Formulário inválido os valores são iguais.";
+    }
+    
+    
+    var resultadoElemento = document.createElement('p');
+    resultadoElemento.textContent = resultado;
+    document.body.appendChild(resultadoElemento);
+  });
+  
