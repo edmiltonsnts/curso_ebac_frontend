42cebc63e8582a6ddd97f92ede2f3cc74adfa463
Edmilton Santos <edmiltonsnts@gmail.com>
2023-03-21 20:36:20 -0300

    Add function number

--- exercicio_html_js/main.js
+++ exercicio_html_js/main.js
@@ -6,10 +6,10 @@ document.querySelector('form').addEventListener('submit', function(event) {
     var valor2 = document.getElementById('valor2').value;
     
     
-    if (valor1 > valor2) {
+    if (Number(valor1) > (valor2)) {
       resultado = "Formulário inválido o valor 1 é maior que o valor 2.";
       
-    } else if (valor2 >= valor1) {
+    } else if (Number(valor2) >= (valor1)) {
       resultado = "O formulário e válido pois o valor 2 é maior ou igual ao valor 1.";
   
     } 
